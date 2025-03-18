f5cc30f248960f8a4c7682366a30fa6a37a94f5b
lolkda <73713586+lolkda@users.noreply.github.com>
2025-03-18 16:01:54 +0800

    Update n_jd_task_conc_whx_sign.py

--- n_jd_task_conc_whx_sign.py
+++ n_jd_task_conc_whx_sign.py
@@ -6,9 +6,11 @@ from function.PrintLog import PrintMethod
 """
 cron: 10 6-23/1 * * *
 new Env('店铺刮刮乐');
-scriptEnv:so
-    export login_url 登录面板地址
-    export check_updatedAt_time 再次转换默认16小时
+export N_jd_whxsign_shopUrl="" # 活动url
+export N_jd_whxsign_TaskPin="" # 指定运行Pin (不写默认全部)
+export N_jd_whxsign_Container="" # 容器
+export N_jd_whxsign_Thread="" # 并发的线程数
+export N_jd_whxsign_Black="" # 黑的Cookie
 """
 
 script_path = path.join(path.dirname(__file__), "script", "n_jd_task_conc_whxsign.so")

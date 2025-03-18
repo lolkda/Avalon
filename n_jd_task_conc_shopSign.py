e05436893c306985ff1bc7ad9f6761b95d22a65f
lolkda <73713586+lolkda@users.noreply.github.com>
2025-03-18 15:57:49 +0800

    Update n_jd_task_conc_shopSign.py

--- n_jd_task_conc_shopSign.py
+++ n_jd_task_conc_shopSign.py
@@ -5,18 +5,19 @@ from function.PrintLog import PrintMethod
 
 """
 cron: 10 6-23/1 * * *
-new Env('店铺签到');
-export N_jd_shopSign_Id="" # 活动Id (F8C76481718FA814000365AB85B29892:1000092984:13117366)
-export N_jd_shopSign_TaskPin="" # 指定运行Pin (只能10个)
-export N_jd_shopSign_Container="" # 容器
-export N_jd_shopSign_Thread="" # 并发线程数
+new Env('店铺刮刮乐');
+export N_jd_whxsign_shopUrl="" # 活动url
+export N_jd_whxsign_TaskPin="" # 指定运行Pin (不写默认全部)
+export N_jd_whxsign_Container="" # 容器
+export N_jd_whxsign_Thread="" # 并发的线程数
+export N_jd_whxsign_Black="" # 黑的Cookie
 """
 
-script_path = path.join(path.dirname(__file__), "script", "n_jd_task_conc_shopSign.so")
+script_path = path.join(path.dirname(__file__), "script", "n_jd_task_conc_whxsign.so")
 
 if not path.exists(script_path):
-    PrintMethod().error("请检查脚本是否存在")
+    PrintMethod.error("请检查脚本是否存在")
 
-Task = importlib.import_module("script.n_jd_task_conc_shopSign")
+Task = importlib.import_module("script.n_jd_task_conc_whxsign")
 
 asyncio.run(Task.main())

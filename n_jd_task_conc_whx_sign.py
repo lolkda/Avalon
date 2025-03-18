9ee0ccffbfaf1551362df4e14b322f02131ceb8a
lolkda <73713586+lolkda@users.noreply.github.com>
2025-03-18 15:55:32 +0800

    Update n_jd_task_conc_whx_sign.py

--- n_jd_task_conc_whx_sign.py
+++ n_jd_task_conc_whx_sign.py
@@ -5,13 +5,13 @@ from function.PrintLog import PrintMethod
 
 """
 cron: 10 6-23/1 * * *
-new Env('京东CK检测');
-scriptEnv:
+new Env('店铺刮刮乐');
+scriptEnv:so
     export login_url 登录面板地址
     export check_updatedAt_time 再次转换默认16小时
 """
 
-script_path = path.join(path.dirname(__file__), "script", "n_jd_task_conc_whxsign.py")
+script_path = path.join(path.dirname(__file__), "script", "n_jd_task_conc_whxsign.so")
 
 if not path.exists(script_path):
     PrintMethod.error("请检查脚本是否存在")

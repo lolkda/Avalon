c6b32b97f3249276b6d6b84c5b366f64368f0a35
lolkda <73713586+lolkda@users.noreply.github.com>
2025-03-27 20:03:27 +0800

    Update n_jd_task_conc_openCard.py

--- n_jd_task_conc_openCard.py
+++ n_jd_task_conc_openCard.py
@@ -9,9 +9,11 @@ new Env('入会有礼');
 export N_jd_openCard_shopUrl="" # 店铺链接
 export N_jd_openCard_Min="" # 最少多少奖励入会 (默认50)
 export N_jd_openCard_TaskPin="" # 指定运行Pin (不写默认全部)
+export N_jd_openCard_TaskCookie="" # 指定运行Cookie
 export N_jd_openCard_Container="" # 容器
 export N_jd_openCard_Thread="" # 并发线程数
 export N_jd_openCard_Black="" # 黑的Cookie
+export N_jd_drawShopGift_Wait="" # 间隔时间
 """
 
 script_path = path.join(path.dirname(__file__), "script", "n_jd_task_conc_openCard.so")

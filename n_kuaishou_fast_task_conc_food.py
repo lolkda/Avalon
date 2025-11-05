1116a3310f8148a45cc12c31042f2323116a6bb2
nai <qq529660404@gmail.com>
2025-11-05 16:02:08 +0800

    .

---
+++ n_kuaishou_fast_task_conc_food.py
@@ -0,0 +1,21 @@
+import importlib
+import asyncio
+from os import path
+from utils.script_logger import ScriptLogger
+
+"""
+cron: 10 6-23/1 * * *
+new Env('快手极速饭补广告');
+export n_kuaishou_fast_card="" # 卡密
+export n_kuaishou_fast_food_source="" # cookie源
+export n_kuaishou_fast_food_run_number_Key="" # 运行次数 (默认1)
+"""
+
+script_path = path.join(path.dirname(__file__), "script", "n_kuaishou_fast_task_conc_food.py")
+
+if not path.exists(script_path):
+    ScriptLogger().error("请检查脚本是否存在")
+
+Task = importlib.import_module("script.n_kuaishou_fast_task_conc_food")
+
+asyncio.run(Task.main())

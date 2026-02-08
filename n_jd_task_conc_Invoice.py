ddc1181175dba2a08b92a7b9022048870a9c7a19
nai <qq529660404@gmail.com>
2026-02-08 19:45:39 +0800

    .

--- n_jd_task_conc_Invoice.py
+++ n_jd_task_conc_Invoice.py
@@ -8,11 +8,11 @@ cron: 10 6-23/1 * * *
 new Env('购物信息汇总');
 """
 
-script_path = path.join(path.dirname(__file__), "scripts", "n_jd_task_conc_Invoice.so")
+script_path = path.join(path.dirname(__file__), "script", "n_jd_task_conc_Invoice.so")
 
 if not path.exists(script_path):
     ScriptLogger().error("请检查脚本是否存在")
 
-Task = importlib.import_module("scripts.n_jd_task_conc_Invoice")
+Task = importlib.import_module("script.n_jd_task_conc_Invoice")
 
 asyncio.run(Task.main())
